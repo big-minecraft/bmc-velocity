7dfe1619298801a5fe0a651a620753cfcb54b7ed
wiji <wijigaming.yt@gmail.com>
2024-10-19 14:45:56 -0700

    Add automatic discovery

--- src/main/java/dev.wiji.bigminecraftvelocity/BigMinecraftVelocity.java
+++ src/main/java/dev.wiji.bigminecraftvelocity/BigMinecraftVelocity.java
@@ -1,43 +1,57 @@
 package dev.wiji.bigminecraftvelocity;
 
 import com.google.inject.Inject;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.proxy.ProxyServer;
+import com.velocitypowered.api.proxy.server.ServerInfo;
 import dev.wiji.bigminecraftapi.BigMinecraftAPI;
 import dev.wiji.bigminecraftapi.redis.RedisListener;
 
+import java.net.InetSocketAddress;
 import java.util.UUID;
 import java.util.logging.Logger;
-
 
 @Plugin(id = "bmc-velocity", name = "BigMinecraftVelocity", version = "1.0",
 		url = "https://github.com/kyriji", description = "A connector plugin for BigMinecraft", authors = {"wiji, Kyro"})
 public class BigMinecraftVelocity {
 
-	public final ProxyServer server;
+	public static ProxyServer INSTANCE;
 	public final Logger logger;
-	public final UUID serverID;
 
 	@Inject
 	public BigMinecraftVelocity(ProxyServer server, Logger logger) {
-		this.server = server;
+		INSTANCE = server;
 		this.logger = logger;
+	}
 
-		this.serverID = UUID.randomUUID();
+	@Subscribe
+	public void onProxyInitialize(ProxyInitializeEvent event) {
+		System.out.println("BigMinecraftVelocity loaded");
 
-		logger.info("Hello there! I made my first plugin with Velocity.");
+		INSTANCE.getEventManager().register(this, new EventHandler());
 
 		new Thread(() -> {
+			registerServers();
+
 			BigMinecraftAPI.init();
-			BigMinecraftAPI.getRedisManager().publish("proxy", "Server " + serverID + " has connected!");
 
 			new RedisListener("proxy") {
 				@Override
 				public void onMessage(String message) {
-					System.out.println("Received PROXY message: " + message);
+					registerServers();
 				}
 			};
 		}).start();
+	}
 
+	public void registerServers() {
+		BigMinecraftAPI.getRedisManager().getInstances().forEach(instance -> {
+			if (INSTANCE.getServer(instance.getName()).isPresent()) return;
+
+			InetSocketAddress address = new InetSocketAddress(instance.getIp(), 25565);
+			INSTANCE.registerServer(new ServerInfo(instance.getName(), address));
+		});
 	}
 }
---
+++ src/main/java/dev.wiji.bigminecraftvelocity/EventHandler.java
@@ -0,0 +1,66 @@
+package dev.wiji.bigminecraftvelocity;
+
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.connection.PreLoginEvent;
+import com.velocitypowered.api.event.player.PlayerChooseInitialServerEvent;
+import com.velocitypowered.api.proxy.server.RegisteredServer;
+import dev.wiji.bigminecraftapi.BigMinecraftAPI;
+import dev.wiji.bigminecraftapi.redis.RedisListener;
+
+import java.util.*;
+
+public class EventHandler {
+
+	public EventHandler() {
+
+		new Thread(() -> {
+			new RedisListener("initial-server-response") {
+				@Override
+				public void onMessage(String message) {
+					String[] parts = message.split(" ");
+					UUID player = UUID.fromString(parts[0]);
+					String server = parts[1];
+
+					if (initialServers.containsKey(player)) return;
+					initialServers.put(player, server);
+
+					new Timer().schedule(new TimerTask() {
+						@Override
+						public void run() {
+							initialServers.remove(player);
+						}
+					}, 5000);
+				}
+			};
+		}).start();
+	}
+
+	private final Map<UUID, String> initialServers = new HashMap<>();
+
+	@Subscribe
+	public void onPreConnect(PreLoginEvent event) {
+		if(event.getUniqueId() == null) return;
+
+		System.out.println("PreLoginEvent: " + event.getUniqueId().toString());
+		BigMinecraftAPI.getRedisManager().publish("request-initial-server", event.getUniqueId().toString());
+		System.out.println("Instance servers: " + BigMinecraftAPI.getRedisManager().getInstances().toString());
+	}
+
+	@Subscribe
+	public void onInitialServerChoose(PlayerChooseInitialServerEvent event) {
+		UUID player = event.getPlayer().getUniqueId();
+
+		String serverString = initialServers.get(player);
+
+		System.out.println("Server string: " + serverString);
+
+		if (serverString == null) return;
+
+		initialServers.remove(player);
+
+		System.out.println(BigMinecraftVelocity.INSTANCE.getAllServers().toString());
+
+		Optional<RegisteredServer> optionalServer = BigMinecraftVelocity.INSTANCE.getServer(serverString);
+		optionalServer.ifPresent(event::setInitialServer);
+	}
+}
